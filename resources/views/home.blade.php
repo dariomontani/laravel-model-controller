8e29fb1f79c2719d7ea8f9162ba220af3887fb9c
dariomontani <dariomontani@gmail.com>
2022-02-17 17:25:34 +0100

    visualizzo a schermo i dati dei film

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.base')
 
 @section('content')
+    <div>
+        @foreach ($movies as $movie)
+            <p>{{$movie->title}}</p>
+            <p>{{$movie->original_title}}</p>
+            <p>{{$movie->nationality}}</p>
+            <p>{{$movie->date}}</p>
+            <p>{{$movie->vote}}</p>
+        @endforeach
+    </div>
     home
-@endsection
+@endsection 
